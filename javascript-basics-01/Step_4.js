13a9497518fbab846b8e7c143343e8c700cf08c3
mayssam-Alsakhen <mayssamalsakhen17@gmail.com>
2021-11-23 23:23:33 +0200

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+var name = prompt("Enter your name : ");
+var surname = prompt("Enter your surname : ");
+var city = prompt("Enter your City");
+alert("Name : " + name + "\n" + "Surename : " + surname +"\n" + "City : " + city);
+
