339007984cfa50c320fe685d45c280054e07d52d
mayssam-Alsakhen <mayssamalsakhen17@gmail.com>
2021-11-24 19:12:23 +0200

    Step_6

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -1,4 +1,7 @@
-var num1 = document.getElementById("first_number").innerHTML;
-var num2 = document.getElementById("second_number").innerHTML;
-var rem = num1 % num2 ;
+function fun() {
+var num1 = document.getElementById("first_number");
+var num2 = document.getElementById("second_number");
+var rem = num1.value % num2.value ;
 alert(rem);
+
+}
--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -1,2 +1,11 @@
-var shoe = document.getElementById("shoe_size").innerHTML;
-var birth = document.getElementById("year").innerHTML;
+function fun(){
+    var validate = document.getElementById("validate");
+    var shoe_size = document.getElementById("shoe_size");
+    var year = document.getElementById("year");
+    var mul = shoe_size.value * 2 ;
+    var add1 = mul.value + 5 ;
+    var mul2 = add1.value * 50 ;
+    var sub = mul2.value - year.value ;
+    var add2 = sub.value + 1766 ;
+        alert(add2);
+}
