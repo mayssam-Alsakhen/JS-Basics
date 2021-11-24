cb620e9cddcef829d2f39f817dc2bd7625994104
mayssam-Alsakhen <mayssamalsakhen17@gmail.com>
2021-11-24 20:44:22 +0200

    Step_7

--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -1,11 +1,5 @@
-function fun(){
-    var validate = document.getElementById("validate");
-    var shoe_size = document.getElementById("shoe_size");
-    var year = document.getElementById("year");
-    var mul = shoe_size.value * 2 ;
-    var add1 = mul.value + 5 ;
-    var mul2 = add1.value * 50 ;
-    var sub = mul2.value - year.value ;
-    var add2 = sub.value + 1766 ;
-        alert(add2);
-}
+function fun1(){
+  var shoe_size = document.getElementById("shoe_size");
+  var year = document.getElementById("year");
+    alert((shoe_size.value * 2 + 5 ) * 50 - year.value + 1766);
+  }
