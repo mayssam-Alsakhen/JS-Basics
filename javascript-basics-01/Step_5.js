8bff9f5d574ceb6029dbad9c51db5871de61e698
mayssam-Alsakhen <mayssamalsakhen17@gmail.com>
2021-11-24 17:56:13 +0200

    Step_5

--- javascript-basics-01/Step_5.js
+++ javascript-basics-01/Step_5.js
@@ -1,4 +1,7 @@
-var num1 = prompt("put a number.");
-var num2 = prompt("put an other number.");
-var mul = num1 * num2;
-alert(mul);
+function fun(){
+    var num1 = document.getElementById("first_number");
+    var num2 = document.getElementById("second_number");
+    var validate = document.getElementById("validate");
+    var mul = num1.value * num2.value ;
+    alert(mul);
+}
