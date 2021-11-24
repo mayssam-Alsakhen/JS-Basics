ad498dc592693de9383abcee5d3386db912cd335
mayssam-Alsakhen <mayssamalsakhen17@gmail.com>
2021-11-24 18:18:42 +0200

    Step_8

--- javascript-basics-01/Step_8.js
+++ javascript-basics-01/Step_8.js
@@ -1,10 +1,10 @@
-
-var age = prompt("yuor age ");
-
-if (age > 18) {
-    alert("you are over 18");
-
-}
-else{
-    alertl("you are under 18");
+function fun() {
+ var age = document.getElementById("age");
+ var validate = document.getElementById("validate");
+ if (age.value > 18) {
+     alert("you are over 18");
+ }
+ else{
+     alert("you are under 18")
+ }
 }
