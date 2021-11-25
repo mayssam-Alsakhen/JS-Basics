4fbd6d794be7f1e56c8b12fc7541be925a31a7ad
mayssam-Alsakhen <mayssamalsakhen17@gmail.com>
2021-11-25 18:21:08 +0200

    step 5

---
+++ javascript-basics-02/step 4/step_4.js
@@ -0,0 +1,10 @@
+function clear(){
+    var clea = confirm('clear');
+    return confirm;
+    if(clea){
+        document.getElementById('name').value = "";
+        document.getElementById('city').value = "";
+        document.getElementById('surname').value = "";
+     
+    }
+}
