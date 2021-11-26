5a48c1483c035d8dc77eab76d56179fe9f2c5932
mayssam-Alsakhen <mayssamalsakhen17@gmail.com>
2021-11-26 23:22:51 +0200

    basic3-step2

---
+++ javascript-basics-03/step 2/step_2.js
@@ -0,0 +1,8 @@
+function shohid(status){
+    if(status==='show'){
+        document.getElementById('texte').style.display = "none";
+    }
+    else{
+        document.getElementById('texte').style.display = "block";
+    }
+}
